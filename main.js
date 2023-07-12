724de40abfc0545f00eaef4f5dc35e5dcd004ee8
Oscar Farfan <farfanjuaniasoscardaniel@gmail.com>
2023-07-11 22:52:15 -0500

    Fusion menu desktop y mobile completos

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu")
+const menuEmail = document.querySelector(".navbar-email");;
+const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuHamIcon = document.querySelector(".menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle("inactive");
+    desktopMenu.classList.toggle("inactive"); // con esta linea se quita o pone una clase desde js
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive")
 }
